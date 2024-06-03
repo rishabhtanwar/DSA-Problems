1f5d38feef146716edce94ea751fec9aa8b2dd63
rishabhtanwar <tanwar.rishabh0@gmail.com>
2024-06-03 17:18:48 +0530

    palindrome

--- app/src/main/java/com/example/dsaproblems/Palindrome.java
+++ app/src/main/java/com/example/dsaproblems/Palindrome.java
@@ -4,15 +4,18 @@ public class Palindrome {
 
     public static void main(String[] args){
         String s = "abcdcba";
+        String otherS = "";
         int left =0;
         int right = s.length()-1;
         boolean status = true;
         while(left<right){
             if (s.charAt(left)!=s.charAt(right)){
+
                 status = false;
             }
             left++;
             right--;
+
         }
         if (!status){
             System.out.println("String is not a palindrome");
